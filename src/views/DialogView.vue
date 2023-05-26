7c4aaf56dc03289623df0d931f947f866a189cd8
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-26 16:08:32 +0300

    Update DialogView.vue

--- src/views/DialogView.vue
+++ src/views/DialogView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="openDialog">
     <div class="header">
-      <img class="logo" />
-      <span class="state"></span>
+      <img class="logo" :src="logoUrl" />
+      <div>
+        <h3>{{ title }}</h3>
+        <span class="state">...печатает</span>
+      </div>
+
       <div class="find"></div>
     </div>
-    <div class="msgs">
-        
-    </div>
+    <div class="msgs"></div>
   </div>
 </template>
 <style scoped lang="scss">
 .openDialog {
+  width: 100%;
+  height: 100%;
   .header {
+    display: flex;
+    .logo {
+      width: 2.5rem;
+      border: 1px solid black;
+      border-radius: 2rem;
+      height: 2.5rem;
+    }
   }
 }
 </style>
@@ -20,8 +31,11 @@
 export default {
   data() {
     return {
-     dialogId:0,
-     props:"",
+      title: "Name 1",
+      logoUrl: "",
+      dialogId: 0,
+      props: "",
+      msgs: [{ id: 0, text: "example", date: Date.now() }],
     };
   },
   created() {
@@ -29,12 +43,9 @@ export default {
   },
   methods: {
     load() {
-      
-      this.dialogId = api.getById(this.$route.params.id)[0];
-    
-    }
-    ,
-}
-
-}
+      this.dialogId = this.$route.params.id;
+      console.log(this.dialogId);
+    },
+  },
+};
 </script>
