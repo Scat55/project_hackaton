c32273b678b23038dbf798ea779d7ee5f2186153
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-05-26 16:15:05 +0300

    add: add signIn page

---
+++ src/views/SignInView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="signin">
+    <div class="signin__text">
+      <div class="signin__title">Войдите в аккаунт</div>
+      <div class="signin__subtitle">Введите логин и пароль в форме ниже</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
