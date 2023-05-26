1bfc301b2475aa97d8b3c714c15983186f0c0d2f
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-26 13:50:53 +0300

    s

--- src/components/Msg.vue
+++ src/components/Msg.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        <div class="header">
-            <img class="logo">
-        </div>
-        
+  <div class="msg" :id="id">
+    <div class="header">
+      <img class="logo" :src="urlLogo" />
     </div>
+    <div class="content">{text}</div>
+  </div>
 </template>
 <style>
 
 </style>
 <script>
+export default {
+  props: ["id", "text", "urlLogo"],
+};
 </script>
