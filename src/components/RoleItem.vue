6211621b322272cb3f5d1dbf74813cab399c8ed8
Anton Toneyev <aatoneyev@gmail.com>
2023-05-26 13:24:21 +0300

    added: roleItem, rolesView

---
+++ src/components/RoleItem.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+    <div class="roleItem" :id="id">
+        <div class="image">
+            <img>
+        </div>
+        <div class="item_info">
+            <span class="info_name">{{name}}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
