aef72fcfffc6b6560513d9474edb86560f479cf1
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-26 23:39:54 +0300

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,39 +1,47 @@
 export default function Api() {
   function writeCookie(name, val, expires) {
-    var date = new Date;
+    var date = new Date();
     date.setDate(date.getDate() + expires);
-    document.cookie = name+"="+val+"; path=/; expires=" + date.toUTCString();
+    document.cookie =
+      name + "=" + val + "; path=/; expires=" + date.toUTCString();
   }
-  this.readCookie=(name) => {
-    var matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
+  this.readCookie = (name) => {
+    var matches = document.cookie.match(
+      new RegExp(
+        "(?:^|; )" +
+          name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+          "=([^;]*)"
+      )
+    );
     return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
+  };
   async function query(data, link) {
+    try{
     const response = await fetch("http://26.72.40.57:7000/" + link);
-    const jsonData = await response.json();
+   
+     return await response.json();
+    }
+    catch{
+      return "{answ:false}";
+    }
   }
   //
-this.login=(login, password) =>{
+  this.login = (login, password) => {
     const data = query({ login: login, password: password }, "auth/login");
-    return JSON.parse(data);
-  }
-  this.registration=(email,name, login, password)=> {
+    return data;
+  
+  };
+  this.registration = (email, name, login, password) => {
     const data = query(
       {
-        name:name,
-        email:email,
+        name: name,
+        email: email,
         login: login,
         password: password,
       },
       "auth/registration"
     );
-    return JSON.parse(data);
-  }
- this.User=()=>
-  {
-    
-
-  }
+    return data;
+  };
+  this.User = () => {};
 }
