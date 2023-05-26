cc295afd847237a1c592900447b313ca7183ff26
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-05-26 16:56:17 +0300

    add: added all elements of signIn page

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="signin">
-    <div class="signin__text">
-      <div class="signin__title">Войдите в аккаунт</div>
-      <div class="signin__subtitle">Введите логин и пароль в форме ниже</div>
+  <div class="signin__window">
+    <div class="signin">
+      <div class="signin__text">
+        <div class="signin__title">Войдите в аккаунт</div>
+        <div class="signin__subtitle">Введите логин и пароль в форме ниже</div>
+      </div>
+
+      <form action="#" class="signin__form">
+        <label>Имя</label>
+        <input class="form__name" type="text" placeholder="Введите имя пользователя">
+        <label>Пароль*</label>
+        <input class="form__passowrd" type="password" placeholder="Введите пароль">
+        <a href="#" class="form__link">Восстановить пароль</a>
+        <button class="form__btn">Войти</button>
+        <p class="form__text">Нет аккаунта?<strong><a href="#" class="form__registr">Зарегистрироваться</a></strong></p>
+      </form>
     </div>
   </div>
 </template>
@@ -11,4 +23,27 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.signin__window {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+}
+
+.signin {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+
+  &__title {
+    text-align: center;
+  }
+
+  &__form {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
