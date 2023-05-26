82ada86145f95cb267b7441783bf2fc0d13adabe
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-26 23:15:02 +0300

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,15 @@
 export default function Api(data) {
+  function writeCookie(name, val, expires) {
+    var date = new Date;
+    date.setDate(date.getDate() + expires);
+    document.cookie = name+"="+val+"; path=/; expires=" + date.toUTCString();
+  }
+  function readCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
   async function query(data, link) {
     const response = await fetch("http://26.72.40.57:7000/" + link);
     const jsonData = await response.json();
@@ -6,6 +17,7 @@ export default function Api(data) {
   //
   function login(login, password) {
     const data = query({ login: login, password: password }, "auth/login");
+    
     return JSON.parse(data);
   }
   function registration(email,name, login, password) {
