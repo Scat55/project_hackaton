fc5f1c676d5c227a5e48c728cd3d5d70fe5d96c4
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-27 01:32:53 +0300

    навигация сделана

--- src/Api.js
+++ src/Api.js
@@ -1,54 +1,52 @@
 export default function Api() {
-  function writeCookie(name, val) {
-    document.cookie = name + "=" + val + ";";
+  function saveToken( val) {
+  localStorage.setItem("token",val)
   }
 
-  function readCookie(name) {
-    var matches = document.cookie.match(
-      new RegExp(
-        "(?:^|; )" +
-          name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
-          "=([^;]*)"
-      )
-    );
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+  function getLocalToken() {
+    return localStorage.getItem("token")
   }
   function deleteAllCookies() {
-    const cookies = document.cookie.split(";");
-
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const eqPos = cookie.indexOf("=");
-        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+      document.cookie =
+        name + "=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     }
-}
+  }
 
-  async function query(data, link) {  
-        return fetch('https://jsonplaceholder.typicode.com/posts',
-        {
-          method: "POST",
-          body: data,
-        })
-        .then((response) => response.json())
-        .then((responseData) => {
-          console.log(responseData);
-          return responseData;
-        })
-        .catch(error => {console.warn(error);return false;});
-      
+  async function query(data, link) {
+    return fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        return responseData;
+      })
+      .catch((error) => {
+        console.warn(error);
+        return false;
+      });
   }
   this.checkToken = () => {
-    return readCookie("token");
+    return getLocalToken();
   };
 
   this.login = async (login, password) => {
-    writeCookie("token", "asdasd");
-    const data = await query({ login: login, password: password }, "auth/login");
-    return data;
+    return query({ login: login, password: password }, "auth/login")
+      .then((response) => saveToken(response.id))
+      .catch((err) => alert(err));
   };
 
-  this.logOut =()=>{deleteAllCookies()};
+  this.logOut = () => {
+    localStorage.clear();
+    location. reload();
+  };
   this.registration = (email, name, login, password) => {
     const data = query(
       {
