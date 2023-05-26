bd5773aa8e212142c6366b75cce528a0b371b844
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-05-26 13:35:25 +0300

    hi

---
+++ src/components/v-chat.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="chat">
+    <aside class="aside">
+      <button class="aside__btn">
+        <img class="aside__btn-plus" src="../assets/images/plus.svg" alt="Plus">Новый чат
+      </button>
+    </aside>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'chat',
+
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.chat {
+  display: flex;
+}
+
+.aside {
+  background-color: #161515;
+  width: 286px;
+  height: 100vh;
+  color: #fff;
+  padding-top: 16px;
+
+  &__btn {
+    display: flex;
+    align-items: center;
+    width: 80%;
+    background-color: transparent;
+    color: #fff;
+    border: 1px solid #fff;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    border-radius: 8px;
+    padding: 10px;
+    margin-left: 10px;
+    outline: none;
+    cursor: pointer;
+    transition: all .3s;
+
+    &:hover {
+      transform: scale(1.04);
+    }
+
+    &-plus {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import DialogView from '../views/DialogView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/:id',
+    name: 'openDialog',
+    component: DialogView,
   },
   {
     path: '/about',
@@ -13,15 +12,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
-]
+    component: function () {},
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
