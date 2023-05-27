2af89af8cd59ed610715d348feb7635b4ddce5d8
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-27 13:25:31 +0300

    Update dialogItem.vue

---
+++ src/components/dialogItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dialog" :id="id">
+    <img />
+    <div>
+      <h3>{{ name }}</h3>
+      <label>{{ status }}</label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["id", "name", "status"],
+};
+</script>
