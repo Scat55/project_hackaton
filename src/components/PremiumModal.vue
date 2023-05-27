842f2262849752a6ab9315e1f5e9f60a606c2c88
Anton Toneyev <aatoneyev@gmail.com>
2023-05-27 23:16:48 +0300

    fix api

--- src/components/PremiumModal.vue
+++ src/components/PremiumModal.vue
@@ -8,12 +8,19 @@
             <div class="premium_content">
                 <div class="title">
                     <span>Премиум GachiChat</span>
+                    <button class="close"><img></button>
                 </div>
                 <div class="text">
                     <span>Оформите подписку и получите полный доступ к Правильному Чату</span>
                 </div>
                 <div class="cards">
-                    
+                    <div class="level1">
+                        <span class="big_span">3</span>
+                        <span class="medium_span"></span>
+                        <span class="price"></span>
+                    </div>
+                    <div class="level2"></div>
+                    <div class="level3"></div>
                 </div>
             </div>
         </div>
