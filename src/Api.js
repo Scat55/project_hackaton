d14259a02a037f1998de2c5565c3ed3fcda0c7d9
Anton Toneyev <aatoneyev@gmail.com>
2023-05-27 14:28:42 +0300

    fix api query

--- src/Api.js
+++ src/Api.js
@@ -20,8 +20,18 @@ export default function Api() {
 
   async function query(data, link) {
     return fetch("http://26.72.40.57:7000/" +link, {
-      method: "POST",
-      body: JSON.stringify(data),
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+  
     })
       .then((response) => response.json())
       .then((responseData) => {
@@ -87,7 +97,7 @@ export default function Api() {
   this.Msgs = () => {
     this.Get = (dialogueId) => {
       return query({token:getLocalToken(), id:dialogueId},
-      "dialogues//messages"
+      "dialogues/"+ dialogueId +"/messages"
       )
     };
     this.New = () => {
