8fad6e1092a39815819e1596cd27fd1ef19d27aa
Anton Toneyev <aatoneyev@gmail.com>
2023-05-28 00:33:58 +0300

    todo modal premium

--- src/components/PremiumModal.vue
+++ src/components/PremiumModal.vue
@@ -8,7 +8,7 @@
             <div class="premium_content">
                 <div class="title">
                     <span class="premium">Премиум </span><span class="gachi">Gachi </span><span class="chat">Chat</span>
-                    <button class="close"><img></button>
+                    <button class="close"><img src="../assets/images/close.svg"></button>
                 </div>
                 <div class="text">
                     <span>Оформите подписку и получите полный доступ к Правильному Чату</span>
@@ -24,13 +24,13 @@
                         <span class="medium_span">месяцев</span>
                         <span class="price">359 р</span>
                     </div>
-                    <div class="level3">
+                    <div class="level1">
                         <span class="big_span">1</span>
                         <span class="medium_span">год</span>
                         <span class="price">699 р</span>
                     </div>
                 </div>
-                <div>
+                <div class="buttons">
                     <button class="subscribe">Подключить</button>
                     <button class="subscribe">Подключить</button>
                     <button class="subscribe">Подключить</button>
@@ -53,6 +53,8 @@
         height: 100%;
         flex-direction: column;
         justify-content: center;
+        left: 210px;
+        top: 186px;
         .bg{
             position: absolute;
             z-index: 998;
@@ -75,6 +77,8 @@
                 padding: 1rem;
                 background-color: #F4EEEE;
                 border-radius: 0.3rem;
+                width: 968px;
+                height: 713px;
                 
 
                 .title{
@@ -114,6 +118,102 @@
                         text-align: right;
 
                         color: #ED5761;
+                    }
+
+                    .close{
+
+                    }
+                }
+
+                .text{
+                    font-family: 'Inter';
+                    font-style: normal;
+                    font-weight: 900;
+                    font-size: 24px;
+                    line-height: 29px;
+
+                    
+
+                    color: #404B62;
+                }
+
+                .cards{
+                    display: flex;
+                    flex-direction: row;
+                    align-items: flex-start;
+                    padding: 12px 0px;
+                    gap: 64px;
+
+                    .level1{
+                        display: flex;
+                        flex-direction: column;
+                        align-items: flex-start;
+                        padding: 24px;
+                        gap: 24px;
+
+                        width: 264px;
+                        height: 320px;
+
+                        /* Hovered */
+
+                        background: #F4EEEE;
+                        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                        border-radius: 8px;
+
+                        .big_span{
+
+                        }
+                    }
+
+                    .level2{
+                        display: flex;
+                        flex-direction: column;
+                        align-items: flex-start;
+                        padding: 24px;
+                        gap: 24px;
+
+                        width: 264px;
+                        height: 320px;
+
+                        background: #ED5761;
+                        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                        border-radius: 8px;
+
+                    }
+                }
+
+                .buttons{
+                    display: flex;
+                    flex-direction: row;
+                    align-items: flex-start;
+                    padding: 12px 0px;
+                    gap: 64px;
+                    
+                    button{
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: center;
+                        align-items: flex-start;
+                        padding: 24px;
+                        width: 264px;
+                        height: 49px;
+                        background: #ED5761;
+                        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                        border-radius: 8px;
+
+                        font-family: 'Inter';
+                        font-style: normal;
+                        font-weight: 500;
+                        font-size: 24px;
+                        line-height: 29px;
+                        /* identical to box height */
+
+                        text-align: right;
+
+                        /* Backgraund */
+
+                        color: #FFFFFF;
+
                     }
                 }
             }
