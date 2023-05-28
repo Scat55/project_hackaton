d20c5e018b5f13810f0887fbf734559ad758cd62
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-05-28 09:51:59 +0300

    Update dialogItem.vue

--- src/components/dialogItem.vue
+++ src/components/dialogItem.vue
@@ -1,36 +1,71 @@
 <template>
   <div class="dialogItem" :class="isActive ?? active" :id="id">
-    <img src="/icons/iconRole.svg" />
-    <div>
+    <img src="/icons/iconRole.svg" @click="Cl"/>
+    <div class="text" @click="Cl">
       <h4>{{ name }}</h4>
       <label>{{ status }}</label>
+    
     </div>
+    <img class="trash" @click="deleteD" src="@/assets/images/trash.svg" alt="">
   </div>
 </template>
 <script>
+import Api from '@/Api';
+import router from '@/router';
+
 export default {
   data() {
-    return {};
+    return {
+    };
   },
-  props: ["id", "name", "status", "isActive"],
+  props: ["id", "name", "status", "isActive","onClick"],
   created() {
     sessionStorage.setItem("dialog" + this.id, this.name);
+ 
   },
-  methods: {},
+  methods:{
+    Cl(){
+      this.onClick(this.id)
+;    },
+    deleteD() {
+    const api  = new Api();
+    api.Dialogs.Delete(this.id).then((result) => {
+      console.log(result);
+      //router.push("/");
+     // router.go(0);
+    }).catch((err) => {
+      
+    });
+    
+   
+  },
+  },
+ 
+  
+
 };
 </script>
 <style scoped lang="scss">
 .dialogItem {
-  padding-left: 1rem;
+  padding-left: 0.5rem;
   display: flex;
   flex-direction: row;
   align-items: center;
-
+  width: 100%;
   color: white;
-  div {
+  position: relative;
+  .text {
+    margin-right: 2.5rem;
   }
   &:hover {
     background: #302e2e;
+  }
+  .trash{
+    position: absolute;
+    right: 1rem;
+    float: right;
+    cursor: pointer;
+   
   }
 }
 .active {
